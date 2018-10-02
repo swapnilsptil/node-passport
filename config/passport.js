c928d75abb9f5e69b6ccb011410588a3f6ef3388
swapnilsptil <swapnils.ptil@gmail.com>
2018-10-02 23:41:01 +0530

    make it better

--- config/passport.js
+++ config/passport.js
@@ -83,7 +83,7 @@ module.exports = function(passport){
                     console.log('------ Profile from FB -----------',profile);
                     newUser.facebook.id = profile.id;
                     newUser.facebook.token = accessToken;
-                    newUser.facebook.name = profile.name.givenName + ' ' + profile.name.familyName;
+                    newUser.facebook.name = profile.name.first_name + ' ' + profile.name.last_name;
                     newUser.facebook.email = profile.emails[0].value;
 
                     newUser.save(function(err){
