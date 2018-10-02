d0901bd5d1241cc8261638d63186da36821c2f5d
swapnilsptil <swapnils.ptil@gmail.com>
2018-10-02 23:50:50 +0530

    make it better

--- routes/routes.js
+++ routes/routes.js
@@ -39,10 +39,14 @@ module.exports = function(app, passport){
 
     app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email'] }));
 
-    app.get('/auth/facebook/callback', passport.authenticate('facebook', { 
-        successRedirect: '/profile',
-        failureRedirect: '/' 
-    }));
+    // app.get('/auth/facebook/callback', passport.authenticate('facebook', { 
+    //     successRedirect: '/profile',
+    //     failureRedirect: '/' 
+    // }));
+
+    app.get('/auth/facebook/callback', passport.authenticate('facebook', function(err, user, info){
+        console.log(err, user, info);
+    }))
 }
 
 function isLoggedIn(req, res, next){
