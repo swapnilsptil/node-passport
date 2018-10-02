82cb0a5ad9c38aa0ec1f3b8f1240f2d3e54871cb
swapnilsptil <swapnils.ptil@gmail.com>
2018-10-02 21:02:12 +0530

    make it better

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,7 @@ module.exports = {
     facebookAuth : {
         clientID : '1908539079240848',
         clientSecret : 'ec2d478d57d6216ca7c0846b12561440',
-        callbackURL : 'https://localhost:3000/auth/facebook/callback'
+        callbackURL : 'https://node-passport.herokuapp.com/auth/facebook/callback'
     }
 
 }
