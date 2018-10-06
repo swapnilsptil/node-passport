cd547edd4d4941baef202509d8f408e06d99a72d
swapnilsptil <swapnils.ptil@gmail.com>
2018-10-06 14:32:19 +0530

    Heroku deployment

--- routes/routes.js
+++ routes/routes.js
@@ -38,7 +38,11 @@ module.exports = function(app, passport){
         res.redirect('/');
     })
 
-    app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email'] }));
+    app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email'] }), 
+            (req, res)=>{
+                res.redirect('api/facebook/profile')
+            }
+    );
 
     app.get('/auth/facebook/callback', passport.authenticate('facebook', { 
         successRedirect: '/profile',
