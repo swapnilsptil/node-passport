853b89fb3d775823938b3dba7cd97838362b00c5
swapnilsptil <swapnils.ptil@gmail.com>
2018-10-08 01:13:01 +0530

    deploying to heroku

--- config/passport.js
+++ config/passport.js
@@ -72,33 +72,53 @@ module.exports = function(passport){
             clientID: configAuth.facebookAuth.clientID,
             clientSecret: configAuth.facebookAuth.clientSecret,
             callbackURL: configAuth.facebookAuth.callbackURL,
-            profileFields: ['id', 'emails', 'name']
+            profileFields: ['id', 'emails', 'name'],
+            passReqToCallback: true
     }, function(accessToken, refreshToken, profile, done) {
         process.nextTick(function(){
-            User.findOne({'facebook.id': profile.id}, function(err, user){
-                if(err)
-                    return done(err);
-                if(user){
-                    console.log('------ Profile from FB -----------',profile);
+            //  User is not logged
+            if(!req.user){
+                User.findOne({'facebook.id': profile.id}, function(err, user){
+                    if(err)
+                        return done(err);
+                    if(user){
+                        console.log('------ Profile from FB -----------',profile);
+                        return done(null, user);
+                    } else {
+                        var newUser = new User();
+                        console.log('------ Profile from FB -----------',profile);
+                        newUser.facebook.id = profile.id;
+                        newUser.facebook.token = accessToken;
+                        newUser.facebook.name = profile._json.first_name + ' ' + profile._json.last_name;
+                        newUser.facebook.email = profile.emails[0].value;
+                        // newUser.facebook.profileUrl = profile.profileUrl;
+                        // newUser.facebook.gender = profile.gender;
+                        // newUser.facebook.displayName = profile.displayName;
+                        
+                        newUser.save(function(err){
+                            if(err)
+                                throw err;
+                            return done(null, newUser);
+                        });
+                    }
+                })
+            } 
+            // User is already logged in with other strategy , need to merge
+            else {
+                var user = req.user;
+                user.facebook.id = profile.id;
+                user.facebook.token = accessToken;
+                user.facebook.name = profile._json.first_name + ' ' + profile._json.last_name;
+                user.facebook.email = profile.emails[0].value;
+
+                user.save(function(err){
+                    if(err)
+                        throw err;
+
                     return done(null, user);
-                } else {
-                    var newUser = new User();
-                    console.log('------ Profile from FB -----------',profile);
-                    newUser.facebook.id = profile.id;
-                    newUser.facebook.token = accessToken;
-                    newUser.facebook.name = profile._json.first_name + ' ' + profile._json.last_name;
-                    newUser.facebook.email = profile.emails[0].value;
-                    // newUser.facebook.profileUrl = profile.profileUrl;
-                    // newUser.facebook.gender = profile.gender;
-                    // newUser.facebook.displayName = profile.displayName;
-                    
-                    newUser.save(function(err){
-                        if(err)
-                            throw err;
-                        return done(null, newUser);
-                    });
-                }
-            })
+                })
+
+            }
         })
     }));
 
