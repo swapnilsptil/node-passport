4244e652459a3262ba3f1d9a7a34704e937021c9
swapnilsptil <swapnils.ptil@gmail.com>
2018-10-08 01:34:40 +0530

    deploying to heroku

--- config/passport.js
+++ config/passport.js
@@ -28,14 +28,14 @@ module.exports = function(passport){
                     if(err){
                         throw err;
                     } 
-                    
                     if(user) {
                         return done(null, false, req.flash('signupMessage','Email already exist'))
-                    } else {
+                    } 
+                    if(!req.user){
                         var newUser = new User();
                         newUser.local.username = email;
                         newUser.local.password = newUser.generateHash(password);
-
+    
                         newUser.save(function(err){
                             if(err){
                                 throw err;
@@ -43,7 +43,19 @@ module.exports = function(passport){
                                 return done(null, newUser);
                             }
                         })
+                    } else {
+                        var user = req.user;
+                        user.local.username = email;
+                        user.local.password = newUser.generateHash(password);
+
+                        user.save(function(err){
+                            if(err)
+                                throw err;
+
+                            return done(null, user);
+                        })
                     }
+                    
             })
         })
     }))
@@ -125,33 +137,49 @@ module.exports = function(passport){
     passport.use(new GoogleStrategy({
         clientID: configAuth.googleAuth.clientID,
         clientSecret: configAuth.googleAuth.clientSecret,
-        callbackURL: configAuth.googleAuth.callbackURL
-    }, function(accessToken, refreshToken, profile, done) {
+        callbackURL: configAuth.googleAuth.callbackURL,
+        passReqToCallback: true
+    }, function(req, accessToken, refreshToken, profile, done) {
         process.nextTick(function(){
-            User.findOne({'google.id': profile.id}, function(err, user){
-                if(err)
-                    return done(err);
-                if(user){
-                    console.log('------ Profile from Google , User found -----------',profile);
+            if(!req.user){
+                User.findOne({'google.id': profile.id}, function(err, user){
+                    if(err)
+                        return done(err);
+                    if(user){
+                        console.log('------ Profile from Google , User found -----------',profile);
+                        return done(null, user);
+                    } else {
+                        var newUser = new User();
+                        console.log('------ Profile from Google , New User -----------',profile);
+                        newUser.google.id = profile.id;
+                        newUser.google.token = accessToken;
+                        newUser.google.name = profile.displayName;
+                        newUser.google.email = profile.emails[0].value;
+                        // newUser.facebook.profileUrl = profile.profileUrl;
+                        // newUser.facebook.gender = profile.gender;
+                        // newUser.facebook.displayName = profile.displayName;
+                        
+                        newUser.save(function(err){
+                            if(err)
+                                throw err;
+                            return done(null, newUser);
+                        });
+                    }
+                })
+            } else {
+                var user = req.user;
+                user.google.id = profile.id;
+                user.google.token = accessToken;
+                user.google.name = profile.displayName;
+                user.google.email = profile.emails[0].value;
+
+                user.save(function(err){
+                    if(err)
+                        throw err;
+
                     return done(null, user);
-                } else {
-                    var newUser = new User();
-                    console.log('------ Profile from Google , New User -----------',profile);
-                    newUser.google.id = profile.id;
-                    newUser.google.token = accessToken;
-                    newUser.google.name = profile.displayName;
-                    newUser.google.email = profile.emails[0].value;
-                    // newUser.facebook.profileUrl = profile.profileUrl;
-                    // newUser.facebook.gender = profile.gender;
-                    // newUser.facebook.displayName = profile.displayName;
-                    
-                    newUser.save(function(err){
-                        if(err)
-                            throw err;
-                        return done(null, newUser);
-                    });
-                }
-            })
+                })
+            }
         })
     }));
 
